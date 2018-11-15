b5cedec4cc3c1e071c7830e51243030fd2c4bd0c
Erasmo Solazzo <esolazzo@codearchitects.com>
2018-11-15 10:34:08 +0100

    Salvataggio su file

--- TestFlatBuffer/Program.cs
+++ TestFlatBuffer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 
 using TestFlatBuffer.Model;
@@ -15,29 +16,42 @@ namespace TestFlatBuffer
   {
     static void Main(string[] args)
     {
-     // var father = new PersonInstance("Father", null);
-     // var child = new PersonInstance("child", new List<PersonInstance>(new PersonInstance[] { father }));
-     // PersonInstance p = new PersonInstance(
-     // "Figlio",
-     // new List<PersonInstance> {
-     //   father,
-     //   new PersonInstance("Mother",null)
-     // }
-     //);
+      var father = new PersonInstance("Father", null);
+      var child = new PersonInstance("child", new List<PersonInstance>(new PersonInstance[] { father }));
+      PersonInstance expected = new PersonInstance(
+      "Figlio",
+      new List<PersonInstance> {
+        child,
+        new PersonInstance("Mother",null)
+      }
+     );
 
-     // p.Works.Add("L1", 1);
-     // p.NickNames.Add("F1", new PersonInstance("Friend", null));
+      expected.Works.Add("L1", 1);
+      expected.NickNames.Add("F1", new PersonInstance("Friend", null));
 
 
-     // var data = new PersonSerializer().Serialize(p).GetBytes();
+      var data = new PersonSerializer().Serialize(expected).GetBytes();
 
-     // var bufReaded = new ByteBuffer(data);
-     // var dataReceived = Person.GetRootAsPerson(bufReaded);
-     // var c = dataReceived.Name;
-     // var f = dataReceived.Parent(0).Value.Name;
-     // var record = dataReceived.WorksByKey("L1");
-     // NickName record2 = dataReceived.NickNamesByKey("F1").Value;
-     // Person a = record2.Value.Value;
+      using (BinaryWriter writer = new BinaryWriter(File.Open("file", FileMode.Create)))
+      {
+        writer.Write(data);
+      }
+      byte[] read;
+
+      using (BinaryReader reader = new BinaryReader(File.Open("file",FileMode.Open)))
+      {
+        read = reader.ReadBytes((int)new FileInfo("file").Length);
+      }
+
+      // var data = new PersonSerializer().Serialize(p).GetBytes();
+
+      // var bufReaded = new ByteBuffer(data);
+      // var dataReceived = Person.GetRootAsPerson(bufReaded);
+      // var c = dataReceived.Name;
+      // var f = dataReceived.Parent(0).Value.Name;
+      // var record = dataReceived.WorksByKey("L1");
+      // NickName record2 = dataReceived.NickNamesByKey("F1").Value;
+      // Person a = record2.Value.Value;
     }
   }
 }
